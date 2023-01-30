3575319f20301ddad991706a6b5fb3c76887b629
Danil Chernov <d.chernov.personal@gmail.com>
2023-01-30 22:04:27 +0700

    feat: added pizza constructor components

---
+++ frontend/src/modules/constructor/DoughSelector.vue
@@ -0,0 +1,117 @@
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  items: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <div class="content__dough">
+    <div class="sheet">
+      <h2 class="title title--small sheet__title">Выберите тесто</h2>
+
+      <div class="sheet__content dough">
+        <label
+          v-for="doughType in items"
+          :key="doughType.id"
+          class="dough__input dough__input--light"
+          :class="`dough__input dough__input--${doughType.value}`"
+        >
+          <input
+            type="radio"
+            name="dought"
+            :value="doughType.value"
+            :checked="doughType.value === modelValue"
+            class="visually-hidden"
+            @input="emit('update:modelValue', doughType.value)"
+          />
+          <b>{{ doughType.name }}</b>
+          <span>{{ doughType.description }}</span>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/ds-system/ds.scss";
+@import "@/assets/scss/mixins/mixins.scss";
+
+.content__dough {
+  width: 527px;
+  margin-top: 15px;
+  margin-right: auto;
+  margin-bottom: 15px;
+}
+
+.dough__input {
+  position: relative;
+
+  margin-right: 8%;
+  margin-bottom: 20px;
+  padding-left: 50px;
+
+  cursor: pointer;
+
+  b {
+    @include r-s16-h19;
+
+    &::before {
+      @include p_center-v;
+
+      width: 36px;
+      height: 36px;
+
+      content: "";
+      transition: 0.3s;
+
+      border-radius: 50%;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+    }
+  }
+
+  span {
+    @include l-s11-h13;
+
+    display: block;
+  }
+
+  &--light {
+    b {
+      &::before {
+        background-image: url("@/assets/img/dough-light.svg");
+      }
+    }
+  }
+
+  &--large {
+    b {
+      &::before {
+        background-image: url("@/assets/img/dough-large.svg");
+      }
+    }
+  }
+
+  &:hover {
+    b::before {
+      box-shadow: $shadow-regular;
+    }
+  }
+
+  input {
+    &:checked + b::before {
+      box-shadow: $shadow-large;
+    }
+  }
+}
+</style>
