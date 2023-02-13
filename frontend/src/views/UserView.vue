2a242454418dffc84b25d4329fd0c26d2986d926
Danil Chernov <d.chernov.personal@gmail.com>
2023-02-13 20:45:45 +0700

    feat: adding new pages

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>UserView.vue</div>
+  <sidebar-layout>
+    <template #sidebar>
+      <RouterLink
+        class="layout__link"
+        :class="{ 'layout__link--active': routeName === 'orders' }"
+        :to="{ name: 'orders' }"
+      >
+        История заказов
+      </RouterLink>
+      <RouterLink
+        class="layout__link"
+        :class="{ 'layout__link--active': routeName === 'profile' }"
+        :to="{ name: 'profile' }"
+      >
+        Мои данные
+      </RouterLink>
+    </template>
+
+    <router-view />
+  </sidebar-layout>
 </template>
+
+<script setup>
+import SidebarLayout from "@/layouts/SidebarLayout.vue";
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+const routeName = computed(() => route.name);
+</script>
